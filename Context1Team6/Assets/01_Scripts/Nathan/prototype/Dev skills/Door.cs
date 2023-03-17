11471a061d18c37004b1314a94d73293dc82b08f
Bryan <dinsbachbryan@gmail.com>
2023-03-18 00:21:53 +0100

    Auto stash before merge of "Development" and "origin/Development"

--- Context1Team6/Assets/01_Scripts/Nathan/prototype/Dev skills/Door.cs
+++ Context1Team6/Assets/01_Scripts/Nathan/prototype/Dev skills/Door.cs
@@ -20,7 +20,6 @@ public class Door : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
         _startPos = DoorPrefab.transform.position; //Transform van waar dit script aan vast zit.
     }
 
